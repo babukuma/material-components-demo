cf4ca0581ca4ce5e8fd59062367fa21e4303f027
Danny Gim <babukuma@gmail.com>
2017-03-30 17:38:25 +0900

    first commit

---
+++ material-components-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  material-components-demo
+//
+//  Created by GIM DOHYOUNG on 2017/03/30.
+//  Copyright © 2017 Danny Gim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ material-components-demoTests/material_components_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  material_components_demoTests.swift
+//  material-components-demoTests
+//
+//  Created by GIM DOHYOUNG on 2017/03/30.
+//  Copyright © 2017 Danny Gim. All rights reserved.
+//
+
+import XCTest
+@testable import material_components_demo
+
+class material_components_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
